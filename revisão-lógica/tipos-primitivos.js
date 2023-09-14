1933d1c776bf398e2226b37f4aa1af2d64640e20
Yasmin de França <127551306+yasmin-de-franca@users.noreply.github.com>
2023-09-14 10:24:48 -0300

    Updated tipos-primitivos.js

--- revisão-lógica/tipos-primitivos.js
+++ revisão-lógica/tipos-primitivos.js
@@ -1,4 +1,20 @@
 //tipos passados por valor
 // strings, number (int, floats(decimais)), boolean
 
-let minhaVar = "minha string"
+let minhaVar = "minha string";
+let minhaVar2 = 'minha "string" coms aspas duplas';
+let minhaVar3 = "minha 'string' com aspas simples";
+let minhaVar4 = `minha template literal`;
+
+let idade = 18
+let msg = "eu possuo " + idade + " anos"; //concatenação
+let msg2 = `eu possuo ${idade} anos`; //mais simples e moderno. Pouca chance de erro
+
+
+console.log(minhaVar);
+console.log(minhaVar2);
+console.log(minhaVar3);
+console.log(minhaVar4);
+
+console.log(msg);
+console.log(msg2);
