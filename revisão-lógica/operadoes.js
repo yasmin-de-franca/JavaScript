129ff65fcacfad01ff62c313e36e3a2cf650f23b
Yasmin de França <127551306+yasmin-de-franca@users.noreply.github.com>
2023-09-26 11:02:05 -0300

    Created operadoes.js

---
+++ revisão-lógica/operadoes.js
@@ -0,0 +1,62 @@
+//OPERADORES ARITIMÉTICOS
+// +(adição) -(subtração) *(multiplicação) /(divisão) %(resto da divisão, porcentagem) **(potência)
+
+let n1 = 10;
+let n2 = 5;
+
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(n1 % n2);
+console.log(2 ** 3);
+
+
+//OPERDORES DE ATRIBUIÇÃO
+
+let n3 = 25;
+//n3 = n3 + 15;
+n3 **= 2;
+console.log(n3);
+
+
+//INCREMENTO E DECREMENTO
+//usa-se quando deseja-se somar ou diminuir 1
+
+let i = 0;
+//i++;
+//i--;
+//i = i + 1;
+//i += 1;
+//++i;
+//--i;
+console.log(i--);
+console.log(i);
+
+//OPERADORES DE COMPARAÇÃO
+/*
+ igualdade de valor ==
+ igualdade de valor e tpo ===
+ <, >, <=, >=
+ != valores diferentes
+ !== valores e tipos diferentes
+*/
+
+console.log(n1, n2, n3);
+//console.log(n1 == "10"); true
+//console.log(n1 === "10"); false
+//console.log(n1 === 10); true
+//console.log(n1 == 10); true
+
+//console.log(n2 < 10); true
+//console.log(n2 > 10); false
+//console.log(n2 < "10"); true
+//console.log(n2 > "10"); false
+
+//console.log(n1 >= 10); true
+//console.log(n1 <= 10); true
+//console.log(n1 >= 11); false
+//console.log(n1 <= 11); true
+
+//console.log(n1 != "10"); false
+//console.log(n1 !== "10"); true
