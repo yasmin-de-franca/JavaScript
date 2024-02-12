c682cccc403b79c6854bec6cd0aaf9e853cdb6c6
Yasmin de França <127551306+yasmin-de-franca@users.noreply.github.com>
2024-02-12 08:32:00 -0300

    Create objetos.js

---
+++ revisão-lógica/objetos.js
@@ -0,0 +1,26 @@
+/*const nomes = ["Yasmin", "Matheus", "Helena"]
+const idades = [27, 28, 1]
+console.log (nomes[0], idades[0])*/
+
+//sintaxe formal
+//const pessoa = new Object()
+//pessoa.nome = "Helena"
+//pessoa.idade = 4
+//console.log (pessoa)
+//let prop = "nome" //usar como uma propriedade
+//console.log (pessoa[prop])
+//console.log (pessoa.idade)
+//console.log (pessoa["idade"])
+
+
+//sintaxe literal
+const pessoa = {
+    nome: "Helena", 
+    idade: 4
+}
+pessoa.nome = "Helena"
+pessoa.idade = 4
+console.log (pessoa)
+let prop = "nome" //usar como uma propriedade
+console.log (pessoa[prop])
+console.log (pessoa["idade"])
